4a90cbd12422464ace8f1799585bee354106e9df
discolarrence <discolarrence@gmail.com>
2021-06-30 00:22:56 -0400

    mapped corners

--- img.py
+++ img.py
@@ -1,16 +1,48 @@
-from bird import GRAY
 import pygame
 from pygame.locals import *
 
-RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 GRAY = (150, 150, 150)
 
+A1 = {"top_left_x":45, 
+    "top_left_y":455,
+    "top_right_x":195,
+    "top_right_y":165,
+    "bottom_left_x": (),
+    "bottom_left_y": (),
+    "bottom_right_x": (),
+    "bottom_right_y": ()}
+
+A2 = {"top_left_x":85, 
+    "top_left_y":410,
+    "top_right_x":195,
+    "top_right_y":165,
+    "bottom_left_x": (),
+    "bottom_left_y": (),
+    "bottom_right_x": (),
+    "bottom_right_y": ()}
+
+A3 = {"top_left_x":85, 
+    "top_left_y":410,
+    "top_right_x":195,
+    "top_right_y":165,
+    "bottom_left_x": (),
+    "bottom_left_y": (),
+    "bottom_right_x": (),
+    "bottom_right_y": ()}
+
+A4 = {"top_left": (),
+    "top_right": (),
+    "bottom_left": (),
+    "bottom_right": ()}
+
+
 pygame.init()
-w, h = 1150, 470
+w, h = 1200, 480
 screen = pygame.display.set_mode((w, h))
 running = True
 
-img = pygame.image.load('sunburst2.png')
+img = pygame.image.load('sunburst7.jpg')
 img.convert()
 rect = img.get_rect()
 rect.center = w//2, h//2
@@ -23,6 +55,7 @@ while running:
 
     screen.fill(GRAY)
     screen.blit(img, rect)
+    pygame.draw.line(screen, GREEN, (A1["top_left_x"], A1["top_left_y"]), (A2["top_left_x"], A2["top_left_y"]), 3)
 
     pygame.display.flip()
 
